1617cd7e6d744fec299c884b8a15bed0bccf6f8c
Fabii137 <146072923+Fabii137@users.noreply.github.com>
2023-11-24 01:09:57 +0100

    Add files via upload

---
+++ copy-dir/Program.cs
@@ -0,0 +1,35 @@
+﻿class CopyDir
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        string path = @"C:\Users\Anwender\source\repos\CodingCampus_2023.09.VZ.Dornbirn\Fabian";
+        string newPath = @"C:\Users\Anwender\Documents\test";
+
+        CopyProject(path, newPath);
+
+    }
+
+    public static void CopyProject(string path, string newPath)
+    {
+        string[] files = Directory.GetFiles(path);
+        string[] dirs = Directory.GetDirectories(path);
+
+        foreach (var file in files)
+        {
+            if (File.Exists(file))
+            {
+                string destFile = Path.Combine(newPath, Path.GetFileName(file));
+                File.Copy(file, destFile, true);
+            }
+        }
+        foreach (var dir in dirs)
+        {
+            string destDir = Path.Combine(newPath, Path.GetFileName(dir));
+            Directory.CreateDirectory(destDir);
+            CopyProject(dir, destDir);
+        }
+    }
+
+}
